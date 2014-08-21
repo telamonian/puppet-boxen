b881a1eeada6984a9d69cecee496f0a39f13f567
telamonian <mklein@jhu.edu>
2014-08-21 09:04:50 -0400

    got bottle fix working

--- manifests/bottle_fixes.pp
+++ manifests/bottle_fixes.pp
@@ -9,24 +9,22 @@
 #   }
 
 class boxen::bottle_fixes ($formula_titles) {
-  create_resources(boxen::bottle_fix, $formula_titles)
+  if ($formula_titles != [] and $formula_titles != undef) 
+  {
+    boxen::bottle_fix { $formula_titles: 
+      before => Package["${formula_titles}"]
+    }
+  }
 }
 
-define boxen::bottle_fix ($formula_title = undef) {
+define boxen::bottle_fix {
 	# the default behavior for boxen is to use bottles the boxen team brews themselves and hosts on their S3 site, rather than the default homebrew bottles
 	# for "reasons"[https://github.com/boxen/puppet-homebrew/issues/18], this royally screws up installing certain programs (gcc,python,nginx,imagemagick) on older macs and virtual machines. If you have the related problem, your build will fail with something like "Illegal instruction: 4" 
 	# the following resource collector fixes this on a package specific level by taking advantage of a quirk in the homebrew.rb provider
-	if $formula_title != undef
-	{
-	  Package <| (title == $formula_title) and (install_options == undef or install_options == []) |> { 
-	    install_options => [ '--verbose' ]
-	  }
-	}
-	else
-	{
-	# this resource collector does the same as above, but for all homebrew packages. Checking provider against undef is meant to capture packages which use the default provider, has not been tested
-		Package <| (provider == homebrew or provider == undef) and (install_options == undef or install_options == []) |> {
-		  install_options => [ '--verbose' ]
-		}
-	}
+  Package <| title == $title and (install_options == undef or install_options == []) |> { 
+    install_options => [ '--verbose' ]
+  }
+	# this resource collector is meant to do the same as above, but for all homebrew packages. Checking provider against undef is meant to capture packages which use the default provider, seems not to work.
+#  Package <| (provider == homebrew or provider == undef) and (install_options == undef or install_options == []) |> {
+#   install_options => [ '--verbose' ]
 }
